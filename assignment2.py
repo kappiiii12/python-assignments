923f74f0cd3c51ab72f226a2cc2ab3802445fccb
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2024-03-29 21:25:00 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+"""
+1. Write a command to get the Python version you are using.
+
+2. What is PVM?
+
+3. How to create .pyc (Byte Code) file from python source file?
+
+4. How to run a Python file from command prompt or terminal?
+
+5. "Data Types in Python are not keywords" - True or False"""
+
+
+#solution 1 python --version
+#solution 2
+""" the role of python virtual machine is to convert
+byte code instructions into machine code so that the computer can
+execute those machine code instructions and display the final output 
+"""
+#solution 3 python3 -m py_campile filename.py
+#solution 4 python3 filename.py
+#solution 5 true
