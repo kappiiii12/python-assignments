ce538b7c5f4ae9144fd18fa28abad9d3979b5538
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2024-03-31 19:27:32 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+"""
+1. Who developed Python?
+
+2. When was the first version of Python launched?
+
+3. "Python is a platform independent language" - True or False
+
+4. How to make single line comment in a python file?
+
+5. "Python is a statically typed programming language" - True or False
+"""
+#solution 1 Guido van rossum
+#solution 2 1994
+#solution 3 true
+#solution 4 using "#"
+#solution 5 NO ,Python is Dynamically typed language
