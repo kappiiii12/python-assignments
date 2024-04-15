f5a19a1461fb5006d718fa3877d020a026b69343
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2024-04-15 21:46:31 +0530

    Add files via upload

---
+++ assignment15.py
@@ -0,0 +1,32 @@
+"""
+1. Write a python script to print MySirG 5 times on the screen
+
+2. Write a python script to print first 10 natural numbers
+
+3. Write a python script to print first 10 natural numbers in reverse order
+
+4. Write a python script to print first 10 odd natural numbers
+
+5. Write a python script to print first 10 odd natural numbers in reverse order
+"""
+#solution 1
+i=1
+while i<=5:
+    print("my sirg")
+    i=i+1
+i=1
+while i<=10:
+    print(i)
+    i=i+1   
+i=1
+while i<=10:
+    print(11-i)
+    i=i+1   
+i=1
+while i<=10:
+    print(2*i-1)
+    i=i+1   
+i=1
+while i<=10:
+    print(21-2*i)
+    i=i+1       
---
+++ assignment16.py
@@ -0,0 +1,30 @@
+"""
+1. Write a python script to print first 10 even natural numbers
+
+2. Write a python script to print first 10 even natural numbers in reverse order
+
+3. Write a python script to print squares of first 10 natural numbers
+
+4. Write a python script to print cubes of first 10 natural numbers
+
+5. Write a python script to print first 10 multiples of 5"""
+i=1
+while i<=10:
+    print(2*i)
+    i=i+1
+i=1
+while i<=10:
+    print(22-2*i)
+    i=i+1 
+i=1
+while i<=10:
+    print(i**2)
+    i=i+1 
+i=1
+while i<=10:
+    print(i**3)
+    i=i+1  
+i=1
+while i<=10:
+    print(5*i)
+    i=i+1    
