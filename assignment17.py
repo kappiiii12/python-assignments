935f4d4f3ef201dddf889a7b346b5e08bce8bcbc
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2024-04-16 11:06:30 +0530

    Add files via upload

---
+++ assignment17.py
@@ -0,0 +1,31 @@
+"""
+1. Write a python script to print MySirG N times on the screen
+
+2. Write a python script to print first N natural numbers
+
+3. Write a python script to print first N natural numbers in reverse order
+
+4. Write a python script to print first N odd natural numbers
+
+5. Write a python script to print first N odd natural numbers in reverse order"""
+n=int(input("enter the value of n = "))
+i=1
+while i<=n:
+    print("my sirg")
+    i=i+1
+i=1
+while i<=n:
+    print(i)
+    i=i+1   
+i=1
+while i<=n:
+    print(n+1-i)
+    i=i+1   
+i=1
+while i<=n:
+    print(2*i-1)
+    i=i+1   
+i=1
+while i<=n:
+    print((2*n+1)-2*i)
+    i=i+1       
---
+++ assignment18.py
@@ -0,0 +1,31 @@
+"""
+1. Write a python script to print first N even natural numbers
+
+2. Write a python script to print first N even natural numbers in reverse order
+
+3. Write a python script to print squares of first N natural numbers
+
+4. Write a python script to print cubes of first N natural numbers
+
+5. Write a python script to print first 10 multiples of N"""
+n=int(input("enter value of n = "))
+i=1
+while i<=n:
+    print(2*i)
+    i=i+1
+i=1
+while i<=n:
+    print((n*2+2)-2*i)
+    i=i+1 
+i=1
+while i<=n:
+    print(i**2)
+    i=i+1 
+i=1
+while i<=n:
+    print(i**3)
+    i=i+1  
+i=1
+while i<=n:
+    print(5*i)
+    i=i+1    
